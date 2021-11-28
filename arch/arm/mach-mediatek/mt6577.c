c048bcaaf371b8292eeb3b777c73a003f14c54a1
Boris Lysov <arzamas-16@mail.ee>
2021-11-29 01:25:25 +0300

    add mt6577 code stub

---
+++ arch/arm/mach-mediatek/mt6577.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Device Tree support for Mediatek mt6577 SoC Family
+ *
+ * Author: Boris Lysov <arzamas-16@mail.ee>
+ */
+#include <asm/mach/arch.h>
+
+static const char * const mt6577_board_dt_compat[] = {
+	"mediatek,mt6577",
+	"mediatek,mt8317",
+	"mediatek,mt8377",
+	NULL,
+};
+
+DT_MACHINE_START(MEDIATEK_MT6577_DT, "Mediatek mt6577 SoC Family")
+	.dt_compat		= mt6577_board_dt_compat,
+MACHINE_END
